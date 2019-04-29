35a755cc38c2876a710ea561e5fd8a547fb15157
noumann266 <nomankabeerr@gmail.com>
2019-04-29 07:00:24 +0500

    Redirect user after login

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -11,7 +11,6 @@ use DB;
 class ChatController extends Controller
 {
     public function getUserList(){
-//        return [User::where('id' , '<>' , Auth::user()->id)->get() , Auth::user()->id];
         return User::where('id' , '<>' , Auth::user()->id)->get();
     }
     public function getUserChat(Request $request){
@@ -19,33 +18,11 @@ class ChatController extends Controller
         $Messages = Chat::
         where(function($q) use($receiver_id){ $q->where('sender_id' , Auth::user()->id); $q->where('receiver_id' , $receiver_id); })
             ->orWhere(function($q) use($receiver_id){ $q->where('receiver_id' , Auth::user()->id); $q->Where('sender_id' , $receiver_id); })
-//            ->limit(1)->page(1)->get();
             ->orderBy('id' , 'DESC')->paginate(20);
-//        $collection = collect($Messages);// Chat::get();
-//return $collection->sortKeysDesc();
         return $Messages;
-
-
-    }
-
-    public function getUserChat3(Request $request){
-        $receiver_id = 2;
-        $Messages = Chat::
-        where(function($q) use($receiver_id){ $q->where('sender_id' , Auth::user()->id); $q->where('receiver_id' , $receiver_id); })
-            ->orWhere(function($q) use($receiver_id){ $q->where('receiver_id' , Auth::user()->id); $q->Where('sender_id' , $receiver_id); })
-
-//            ->orderBy('id' , 'ASC')->paginate(20);
-       // return $Messages;// Chat::get();
-
-        ->latest()->paginate(20);
-        $collection = collect($Messages);// Chat::get();
-        return  $collection->sortKeysDesc();
-//        return  $collection;
-
     }
     public function storeMessage(Request $request){
         event(new ChatEvent($request));
-//        $receiver_id = $request->receiver->id;
         $request = $request->all();
         unset($request['_token']);
         $request['sender_id'] = Auth::user()->id;
@@ -53,7 +30,6 @@ class ChatController extends Controller
         $receiver_id = $request['receiver_id'];
         Chat::Where(function($q) use($receiver_id){ $q->where('receiver_id' , Auth::user()->id); $q->Where('sender_id' , $receiver_id); })->update(['is_seen' => 1]);
         return Chat::Where(function($q) use($receiver_id){ $q->where('sender_id' , Auth::user()->id); $q->where('receiver_id' , $receiver_id); })->count();
-        return 'success';
     }
     public function getUserAvatar(Request $request){
         return User::find($request->id)->avatar;
@@ -64,5 +40,4 @@ class ChatController extends Controller
     public function deleteMessage(Request $request){
         return Chat::where('id' , $request->id)->delete();
     }
-
 }
