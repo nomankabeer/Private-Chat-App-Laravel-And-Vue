77669f80d9a4d164f85b5c230075fd07f995df70
noumann266 <nomankabeerr@gmail.com>
2019-04-29 05:12:49 +0500

    message notification

--- resources/js/components/UserList.vue
+++ resources/js/components/UserList.vue
@@ -9,7 +9,7 @@
             <div class="about">
                 <div class="name">{{ user.name}}</div>
                 <div class="status">
-                   <span v-if="user.new_message != 0" :style="{display: user_msg_notification}" :id="user.id" class=" old badge-danger badge"> {{user.new_message}}  user model </span>
+                   <span v-if="user.new_message != 0" :style="{display: user_msg_notification}" :id="user.id" class=" old badge-danger badge"> {{user.new_message}}  new </span>
                    <span  :id="'new_'+user.id" class="badge-danger badge">  </span>
                 </div>
             </div>
@@ -49,7 +49,7 @@
             },
             new_message_there: function(value){
                 this.user_msg_notification = 'none';
-                $('#new_'+value.sender_id).css('display' , 'block').html(value.total_unread_messages + ' pusher');
+                $('#new_'+value.sender_id).css('display' , 'block').html(value.total_unread_messages + ' new');
             },
             remove_notification: function(value){
                 setTimeout(function() {
